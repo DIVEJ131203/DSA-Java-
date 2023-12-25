590d7ab1e2303026cf2f61ca82ff70a5ff5d1026
Divej Ahuja <147057830+DIVEJ131203@users.noreply.github.com>
2023-12-25 13:18:45 +0530

    Add files via upload

---
+++ ac1.java
@@ -0,0 +1,13 @@
+public class ac1 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+    }
+}
