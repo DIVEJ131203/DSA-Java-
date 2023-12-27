50637110a3b36ed31b185f834855e2aa3616042c
Divej Ahuja <147057830+DIVEJ131203@users.noreply.github.com>
2023-12-27 21:42:11 +0530

    variables in java

---
+++ JavaBasics2.java
@@ -0,0 +1,10 @@
+public class JavaBasics2 {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 9;
+        System.out.println(a);
+        System.out.println(b);
+        String name = "divej";
+        System.out.println(name);
+    }
+}
