52c76abe372777c6949e7888ed940b14e643c80e
Divej Ahuja <147057830+DIVEJ131203@users.noreply.github.com>
2023-12-27 21:33:55 +0530

    Star Pattern

---
+++ JavaBasic1.java
@@ -0,0 +1,9 @@
+public class JavaBasic1 {
+    public static void main(String[] args) {
+        System.out.println("Hello Divej Here");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+}
