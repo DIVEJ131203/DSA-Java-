160727e76812f80fcc741c6c68c883516bf3d5d7
Divej Ahuja <147057830+DIVEJ131203@users.noreply.github.com>
2023-12-28 18:43:00 +0530

    Practice Set 1

---
+++ practiceset2q2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class practiceset2q2 {
+    public static void main(String[] args) {
+        System.out.println("Enter side of square");
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
+        int area = s * s;
+        System.out.println("Area:" + area);
+        sc.close();
+    }
+}
