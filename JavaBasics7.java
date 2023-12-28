8d85f09a667b02fab6e46b5227abceb25448090c
Divej Ahuja <147057830+DIVEJ131203@users.noreply.github.com>
2023-12-28 18:38:58 +0530

    Type Promotion

---
+++ JavaBasics7.java
@@ -0,0 +1,10 @@
+public class JavaBasics7 {
+
+    public static void main(String[] args) {
+        char a = 'a';
+        char b = 'z';
+        System.out.println(a + b);
+    }
+}
+
+// Type Promotion in java
