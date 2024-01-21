21e7bd8a43968e08ea61adb390f0e62690ebab9e
Divej Ahuja <147057830+DIVEJ131203@users.noreply.github.com>
2024-01-21 16:02:41 +0530

    Bubble Sort Non-Increasing

--- BubbleSortDescending.java
+++ BubbleSortDescending.java
@@ -1,3 +1,14 @@
 public class BubbleSortDescending {
-
+public static void bubbleSortDescending(int arr[])
+{
+    for (int turn=0; turn<arr.length;turn++)
+    {
+        for(int j-0; j<arr.lengt-1-turn;j++)
+        {
+            int temp=arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]=temp;
+        }
+    }
+}
 }
